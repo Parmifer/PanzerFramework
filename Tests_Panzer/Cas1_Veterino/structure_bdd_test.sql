7c13de81ed594de1c09596ccc1dc1b1c1c818eb0
lucile-sticky <lucile@stickyads.tv>
2016-06-15 13:55:26 +0200

    Modification after strange behavior withMaxime's computer.

--- Tests_Panzer/Cas1_Veterino/structure_bdd_test.sql
+++ Tests_Panzer/Cas1_Veterino/structure_bdd_test.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS `panzer_veterino_test`;
 
-CREATE DATABASE IF NOT EXISTS `panzer_veterino_test`;
+CREATE DATABASE IF NOT EXISTS `panzer_veterino_test` 
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
 
 USE `panzer_veterino_test`;
 
@@ -9,7 +11,7 @@ CREATE TABLE `role` (
     `label` VARCHAR(255),
     `level` int NOT NULL UNIQUE,
     `code` VARCHAR(1) NOT NULL UNIQUE
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `user` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
@@ -18,7 +20,7 @@ CREATE TABLE `user` (
     `creation_date` TIMESTAMP DEFAULT NOW(),
     role_id int,
     FOREIGN KEY (role_id) REFERENCES role(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `ressources_humaines` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
@@ -28,7 +30,7 @@ CREATE TABLE `ressources_humaines` (
     `salaire` FLOAT(7,2),
     `user_id` int,
     FOREIGN KEY (user_id) REFERENCES user(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `veterinaire` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
@@ -38,7 +40,7 @@ CREATE TABLE `veterinaire` (
     `salaire` FLOAT(7,2),
     `user_id` int,
     FOREIGN KEY (user_id) REFERENCES user(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `infirmier` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
@@ -48,14 +50,14 @@ CREATE TABLE `infirmier` (
     `salaire` FLOAT(7,2),
     `user_id` int,
     FOREIGN KEY (user_id) REFERENCES user(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `salle` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
     `etage` int,
     `numero` VARCHAR(255) NOT NULL,
     `nombre_places` int
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `chat` (
     `id` int AUTO_INCREMENT PRIMARY KEY,
@@ -65,7 +67,7 @@ CREATE TABLE `chat` (
     `etat` ENUM('entré', 'pris en charge', 'opéré', 'rétablissement', 'ok', 'sorti'),
     `salle_id` int,
     FOREIGN KEY (salle_id) REFERENCES salle(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `prendre_en_charge` (
     `chat_id` int,
@@ -73,7 +75,7 @@ CREATE TABLE `prendre_en_charge` (
     PRIMARY KEY (chat_id, infirmier_id),
     FOREIGN KEY (chat_id) REFERENCES chat(id),
     FOREIGN KEY (infirmier_id) REFERENCES infirmier(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `diagnostiquer` (
     `chat_id` int,
@@ -83,7 +85,7 @@ CREATE TABLE `diagnostiquer` (
     PRIMARY KEY (chat_id, veterinaire_id),
     FOREIGN KEY (chat_id) REFERENCES chat(id),
     FOREIGN KEY (veterinaire_id) REFERENCES veterinaire(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `operer` (
     `chat_id` int,
@@ -92,4 +94,4 @@ CREATE TABLE `operer` (
     PRIMARY KEY (chat_id, veterinaire_id),
     FOREIGN KEY (chat_id) REFERENCES chat(id),
     FOREIGN KEY (veterinaire_id) REFERENCES veterinaire(id)
-);
+) ENGINE=InnoDB;
